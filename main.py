7322306cb52a7f0a509c4f6e5c0393e42da777b7
Carlos Menezes <carlosedusm@gmail.com>
2025-07-02 09:58:14 -0300

    preliminary attempt for tries counting

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from re import findall
 
 
 def main():
-    tries = 1
+    tries = []
     tried_letter = []
     words = list(get_english_words_set(["web2"], lower=True))
 
     def chosen_word(difficulty: list):
+        """Choose a random word based on the difficulty chosen them return a list with
+        the enumerated word, the string with underscores in lenght of the word and 
+        marker of correct guesses.
+        """
         word = choice(difficulty)
-        return [word, "_" * len(word)]
+        return [list(enumerate(word)), "_" * len(word)]
 
     def choosing_difficulty():
+        """Choose a difficulty between 1(Easy), 2(Medium) and 3 (Hard) then return an
+        array with words based in which difficulty was chosen.
+        """
         while True:
             try:
                 difficulty = int(
@@ -37,6 +44,7 @@ def main():
                     print("You didn't choose a valid difficulty. Please try again.")
 
     def verified_input():
+        """Ask for a letter and return that letter if the input is valid."""
         while True:
             letter: str = input("\nPlease choose a letter: ")
             if (
@@ -57,17 +65,40 @@ def main():
                 print("\nSorry, that wasn't a valid input")
 
     def letter_in_word(word: list, letter: str):
-        indexes = list(enumerate(word[0]))
-        new_blank = [w for w in word[1]]
-        for char in list(filter(lambda x: x[1] == letter, indexes)):
-            new_blank[char[0]] = char[1]
-        word[1] = "".join(new_blank)
-        return word
+        """Check if the letter is in the word, if it is change the string with underscores
+        to show the letter then return a list with the word and the new string.
+        """
+        new_blank = [c for c in word[1]]
+        if any(list(map(lambda x: x[1] == letter, word[0]))):
+            for char in list(filter(lambda x: x[1] == letter, word[0])):
+                new_blank[char[0]] = char[1]
+            word[1] = "".join(new_blank)
+            return word
+        else:
+            tries.append('X')
+
+    def win_condition(chances: int, word: list):
+        if "_" not in word[1] and chances < 3:
+            print("\nYou won!")
+            return True
+        else:
+            return False
 
     word = chosen_word(choosing_difficulty())
-    checked_letter = letter_in_word(word, verified_input())
-
-    print(checked_letter)
+    count = ["_", "_", "_"]
+    while True:
+        condition = win_condition(len(tries), word)
+        if condition:
+            break
+        elif len(tries) >= 3:
+            count[len(tries) - 1] = tries[len(tries) - 1]
+            print(f"\nYou Lose!\nTries: {"".join(count)}\nThe word was: {''.join([w[1] for w in word[0]])}")
+            break
+        else:
+            if tries:
+                count[len(tries) - 1] = tries[len(tries) - 1]
+            print(f"\nTries: {"".join(count)}\nThe word: {word[1]}")
+            letter_in_word(word, verified_input())
 
 
 if __name__ == "__main__":
